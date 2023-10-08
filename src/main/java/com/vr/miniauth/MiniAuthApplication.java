c0d03e4a89da30de28e49e7124c58e6241dfe46b
GabrielGiuseppe <gabriel.nascimento@vorttex.com>
2023-10-08 07:49:01 -0300

    commit com estrutura inicial do projeto com spring

---
+++ src/main/java/com/vr/miniauth/MiniAuthApplication.java
@@ -0,0 +1,13 @@
+package com.vr.miniauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAuthApplication.class, args);
+	}
+
+}
